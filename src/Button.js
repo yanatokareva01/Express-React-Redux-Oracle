cb37ef13bd94aa248879ee6b5413936eff972bde
Yana Tokareva <yanatokareva01@gmail.com>
2017-06-18 15:45:29 +0300

    Remove files

--- src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class SignUpButton extends React.Component {
-	render() {
-		return <form method="post" action={this.props.action}>
-			<button className="btn btn-lg btn-primary" type="submit">{this.props.value}</button>
-		</form>
-	}
-}
